15156dc50ca493f5e53288642dd654b73fcd5137
vsol <vsol.sil@gmail.com>
2021-10-18 12:56:21 -0300

    finalizando seccion principal con efecto parallax

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const toggler = document.querySelector('.toggler');
 
 const menu = () => {
-    console.log ('Funciona')
     const navLinks = document.querySelector('#nav-toggler');
     navLinks.classList.toggle('nav__links');
     return;
