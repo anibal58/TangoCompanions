438da51a189fb9bf33ecc3f9682a9603fd029b67
vsol <vsol.sil@gmail.com>
2021-10-18 20:35:14 -0300

    barra de progreso segun scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
+//Efecto toggler para el menu de navegación en tamaño table y mobile
+
 const toggler = document.querySelector('.toggler');
 
 const menu = () => {
     const navLinks = document.querySelector('#nav-toggler');
     navLinks.classList.toggle('nav__links');
+
+    const animationContainer = document.querySelector('.animation');
+
+    if (document.querySelector('.show-animation')) {
+        animationContainer.classList.add('hide-animation');
+        animationContainer.classList.remove('show-animation');
+    } else {
+        animationContainer.classList.add('show-animation');
+        animationContainer.classList.remove('hide-animation');
+    }
+    
     return;
 }
 
 toggler.addEventListener('click', menu);
+
+// Barra de progreso
+const progress = () => {
+    const scrollDistanceFromTop = document.documentElement.scrollTop;
+
+    const documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    const progressPercent= (scrollDistanceFromTop/documentHeight)*100;
+
+    document.getElementsByClassName('barra')[0].style.width = `${progressPercent}%`;
+    console.log(document.getElementsByClassName('barra')[0].style.width = `${progressPercent}%`)
+}
+
+window.addEventListener('scroll', progress)
