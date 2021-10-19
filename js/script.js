16897c17a342cf8a57eede1913257d8410bb12cd
vsol <vsol.sil@gmail.com>
2021-10-18 22:45:55 -0300

    galeria finalizada

--- js/script.js
+++ js/script.js
@@ -30,7 +30,50 @@ const progress = () => {
     const progressPercent= (scrollDistanceFromTop/documentHeight)*100;
 
     document.getElementsByClassName('barra')[0].style.width = `${progressPercent}%`;
-    console.log(document.getElementsByClassName('barra')[0].style.width = `${progressPercent}%`)
 }
 
 window.addEventListener('scroll', progress)
+
+
+//Overlay Galeria
+
+document.addEventListener('DOMContentLoaded', function () {
+    overlayEffect()
+})
+
+const overlayEffect = () => {
+    for (let i=1; i<10; i++) {
+        const imagen = document.querySelector(`.item-${i}`);
+        imagen.onclick = function () {
+            imagenZoomIn (`../img/img${i}.jpg`)
+        }
+    }
+}
+
+function imagenZoomIn (i) {
+    //Crear overlay
+   const overlay = document.createElement('DIV');
+   overlay.innerHTML = `<img src="${i}" alt="tango">`
+   overlay.classList.add('overlay');
+   overlay.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fix-body')
+        overlay.remove();
+    }
+
+   //Boton para cerrar
+   const close = document.createElement('P');
+   close.textContent = 'X';
+   close.classList.add('btn-cerrar')
+   close.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fix-body')
+        overlay.remove();
+   }
+   overlay.appendChild(close);
+
+   //Añadirlo al HTML
+   const body = document.querySelector('body');
+   body.appendChild(overlay);
+   body.classList.add('fix-body')
+}
