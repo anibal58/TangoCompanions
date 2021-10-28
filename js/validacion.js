01dabe04731bf8c9832fd348860c373f4b071772
Anibal Maidana <anibalmaidana30@gmail.com>
2021-10-28 14:09:04 -0300

    Más de validaciones

--- js/validacion.js
+++ js/validacion.js
@@ -1,14 +1,40 @@
-const name = document.getElementById('iname')
-const email = document.getElementById('iemail')
-const request = document.getElementById('irequest')
-const form = document.getElementById('form')
+function validar(){
+  var nombre = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
 
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  var regn =/^[a-zA-Z ]{2,30}$/;  
+  var regx =/\S+@\S+\.\S+/;
+  let flag1 = false;
+  let flag2 = false;
+    
+    
+    if(regn.test(nombre) || nombre != "" ){
+      document.getElementById("texto1").innerHTML="Nombre válido";
+      document.getElementById("texto1").style.visibility="visible";
+      document.getElementById("texto1").style.color= "#33ff00";
+      flag1 = true;
+    } else{
+      document.getElementById("texto1").innerHTML="Nombre inválido";
+      document.getElementById("texto1").style.visibility="visible";
+      document.getElementById("texto1").style.color="#ff0000";
+    }
+    
+    if(regx.test(email) || nombre != ""){
+      document.getElementById("texto2").innerHTML="Email válido";
+      document.getElementById("texto2").style.visibility="visible";
+      document.getElementById("texto2").style.color= "#33ff00";
+      flag2 = true;
+    } else{
+      document.getElementById("texto2").innerHTML="E-mail inválido";
+      document.getElementById("texto1").style.visibility="visible";
+      document.getElementById("texto2").style.color="#ff0000";
+    } 
+
+    if ((flag1 == true) && (flag2 == true)){
+      alert("Enviado");
+      document.getElementById("texto1").style.visibility= "hidden";
+      document.getElementById("texto2").style.visibility="hidden";
+    }
+  
+
 }
-
-function enviarFormulario() {
-  console.log("Enviando formulario...");
-  return false;
-} 
